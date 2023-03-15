0ab0ffbcf249d5f4e36a121f4918d5eb04b8882b
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2023-03-15 05:02:55 -0600

    imported axios
    -tried to get api working

---
+++ app.js
@@ -0,0 +1,27 @@
+import {axios} from 'axios';
+
+let url = 'https://api.adviceslip.com/advice';
+
+axios.get(url).then(displayAdvice);
+
+/*console.log(fetch('https://api.adviceslip.com/advice'));
+
+fetch('https://api.adviceslip.com/advice', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+  body: JSON.stringify ({
+    name: 'user 1'
+  })
+})
+    .then(res => {
+      return  res.json()
+    })
+    
+    .then(data => console.log(data))
+
+    fetch('https://jsonplaceholder.typicode.com/posts/1/')
+      .then(response => response.json())
+      .then(json => console.log(json))*/
+
